191439821ac631a2d29c42fa4e2db663304d532f
Dave Della Costa <ddellacosta@gmail.com>
2020-10-06 21:50:22 -0400

    remove cruft

--- my-app/shell.nix
+++ my-app/shell.nix
@@ -1,18 +1,1 @@
-# { nixpkgs ? import ./pinned.nix {}, withHoogle ? true }:
-# 
-# let
-# #  myHaskellPackages = nixpkgs.haskell.packages.ghc884.override {
-# #    overrides = hself: hsuper: {
-# #      some-lib = hself.callPackage ../some-lib/default.nix { };
-# #    };
-# #  };
-# 
-# #  hpkgs = myHaskellPackages;
-#   hpkgs = nixpkgs.haskell.packages;
-#   my-app = nixpkgs.callPackage ./. {};
-# in
-# my-app.overrideAttrs (drv: {
-#   buildInputs = drv.buildInputs ++ [ nixpkgs.cabal-install ];
-# })
-
-(import ./release.nix { }).shell  
+(import ./release.nix { }).shell
